07cb9ca43bbe8ad7b3a65f0158c64c7a27e4546a
Tyrone Sta. Maria <tyrone.stamaria35@gmail.com>
2021-02-28 01:46:25 +0800

    added sucrose theme :D

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import { Header, Card } from 'react-native-elements';
-import { View } from 'react-native';
+import {Header, Card} from 'react-native-elements';
+import {View, ImageBackground} from 'react-native';
 
-const Layout = ({ children }) => {
+const Layout = ({children}) => {
+  // const image = {uri: ''};
   return (
-    <View>
-      <Header 
-        leftComponent={{ icon:'settings', color:'#fff' }}
-        centerComponent={{ text: 'RESIN TIMER', style:{color:'#fff', fontWeight:'bold'} }}
-      />
-      {children}
-    </View>
+    <>
+      <View style={{flex: 1, flexDirection: 'column'}}>
+        <Header
+          leftComponent={{icon: 'settings', color: '#fff'}}
+          centerComponent={{
+            text: 'RESIN TIMER',
+            style: {
+              fontFamily: 'FredokaOne-Regular',
+              fontSize: 20,
+              color: '#fff',
+              fontWeight: 'bold',
+            },
+          }}
+          backgroundColor="#248082"
+        />
+        <ImageBackground
+          source={require('../assets/images/sucrose.jpg')}
+          style={{flex: 1, resizeMode: 'cover', justifyContent: 'center'}}>
+          {children}
+        </ImageBackground>
+      </View>
+    </>
   );
-}
+};
 
-export default Layout
+export default Layout;
--- src/features/Timer/Timer.js
+++ src/features/Timer/Timer.js
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { Text, Input, Card, Button } from 'react-native-elements';
-import { View } from 'react-native';
-import _ from "lodash";
-import moment from "moment";
-
-
+import React, {useState, useEffect} from 'react';
+import {Text, Input, Card, Button} from 'react-native-elements';
+import {View, Image} from 'react-native';
+import _ from 'lodash';
+import moment from 'moment';
 
 const TOTALRESIN = 160;
 const RESININTERVAL = 8;
@@ -12,46 +10,57 @@ const RESININTERVAL = 8;
 
 const computeTime = (numResins, startTime) => {
   // console.log(startTime)
-  moment(startTime).utcOffset(8).format('HH:mm');
   let resinDiff = TOTALRESIN - numResins;
-  let timeToComplete = resinDiff * RESININTERVAL ;
+  let timeToComplete = resinDiff * RESININTERVAL;
 
   let d = new Date();
-  let timeDifference = parseInt(Math.abs(d.getTime() - startTime.getTime()) / 1000);
+  let timeDifference = parseInt(
+    Math.abs(d.getTime() - startTime.getTime()) / 1000,
+  );
 
-  let hours = parseInt(timeToComplete / 60);
-  let mins = parseInt(timeToComplete % 60);
-
+  let hoursH = parseInt(timeToComplete / 60) - 16;
+  let minsH = parseInt(timeToComplete % 60);
 
   let currHours = parseInt((timeToComplete - timeDifference / 60) / 60);
   let currMins = parseInt((timeToComplete - timeDifference / 60) % 60);
-  let currSecs = parseInt((timeToComplete * 60 - timeDifference) % 60)
+  let currSecs = parseInt((timeToComplete * 60 - timeDifference) % 60);
 
-  let currResins = parseInt(timeDifference / (60 * RESININTERVAL) + parseInt(numResins));
+  let currResins = parseInt(
+    timeDifference / (60 * RESININTERVAL) + parseInt(numResins),
+  );
 
+  if (
+    currResins >= TOTALRESIN &&
+    currHours == 0 &&
+    currMins == 0 &&
+    currSecs == 0
+  ) {
+    return;
+  }
 
   let timeLeft = {
     hours: currHours,
     min: currMins,
     sec: currSecs,
     resins: currResins,
-    time: moment(startTime).add(hours, "hours").add(mins, "mins").format("LT")
-  }
+    time: moment(startTime)
+      .add(hoursH, 'hour')
+      .add(minsH, 'minute')
+      .format('LT'),
+  };
 
-  return timeLeft; 
-
-}
+  return timeLeft;
+};
 
 const Timer = () => {
-  // let time = 
+  // let time =
   const [startTime, setStartTime] = useState(new Date());
-  
+
   const [numResins, setNumResins] = useState(0);
   const [timeLeft, setTimeLeft] = useState({});
-  
 
   useEffect(() => {
-    if(!_.isEmpty(timeLeft)){
+    if (!_.isEmpty(timeLeft)) {
       const timer = setTimeout(() => {
         setTimeLeft(computeTime(numResins, startTime));
       }, 1000);
@@ -60,29 +69,142 @@ const Timer = () => {
       };
     }
   });
-  
 
   return (
-    <Card>
-      <Input placeholder='Current number of resins' 
-        keyboardType='number-pad' 
-        onChangeText={(text)=>{setNumResins(parseInt(text))}} />
+    <Card containerStyle={{opacity: 0.85, height: `90%`}}>
+      <View style={{alignItems: 'center', paddingVertical: 40}}>
+        <Text
+          style={{
+            fontFamily: 'FredokaOne-Regular',
+            fontSize: 36,
+            color: `#248082`,
+            marginBottom: 28,
+          }}>
+          Welcome Traveler
+        </Text>
+        <Input
+          placeholder="Current number of resins"
+          keyboardType="number-pad"
+          onChangeText={(text) => {
+            setNumResins(parseInt(text));
+          }}
+          style={{
+            backgroundColor: `#fff`,
+            borderBottomWidth: 0,
+            borderRadius: 8,
+            shadowColor: '#000',
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.23,
+            shadowRadius: 2.62,
+            elevation: 4,
+            paddingHorizontal: 8,
+          }}
+        />
 
-      <Button title="Calculate" onPress={()=>{
-        setTimeLeft(computeTime(numResins, startTime));
-        setStartTime(new Date());
-      }} />
-      <View style={{ alignItems:`center` }}>
-        <Text h3>Resins</Text>
-        <Text h4>{timeLeft.resins}</Text>
-        <Text h3>Duration</Text>
-        <Text h4> {timeLeft.hours}hrs {timeLeft.min}mins {timeLeft.sec}secs </Text>
-        <Text h4> {timeLeft.time}</Text>
+        <Button
+          title="Calculate"
+          onPress={() => {
+            setTimeLeft(computeTime(numResins, startTime));
+            setStartTime(new Date());
+          }}
+          titleStyle={{fontFamily: `FredokaOne-Regular`, fontSize: 18}}
+          buttonStyle={{
+            paddingVertical: 8,
+            paddingHorizontal: 65,
+            backgroundColor: `#248082`,
+            borderRadius: 8,
+          }}
+        />
       </View>
+      {!_.isEmpty(timeLeft) ? (
+        <View style={{alignItems: `center`}}>
+          <View
+            style={{
+              borderBottomWidth: 2,
+              width: `100%`,
+              alignItems: `center`,
+              borderBottomColor: `#509083`,
+              marginBottom: 16,
+            }}>
+            <Text
+              style={{
+                fontFamily: 'FredokaOne-Regular',
+                fontSize: 24,
+                color: `#509083`,
+              }}>
+              Resins
+            </Text>
+            <Text
+              style={{
+                color: `#509083`,
+                fontSize: 18,
+                marginBottom: 6,
+              }}>
+              {timeLeft.resins}
+            </Text>
+          </View>
+          <View
+            style={{
+              borderBottomWidth: 2,
+              width: `100%`,
+              alignItems: `center`,
+              borderBottomColor: `#509083`,
+              marginBottom: 16,
+            }}>
+            <Text
+              style={{
+                fontFamily: 'FredokaOne-Regular',
+                fontSize: 24,
+                color: `#509083`,
+              }}>
+              Refresh Duration
+            </Text>
+            <Text
+              style={{
+                color: `#509083`,
+                fontSize: 18,
+                marginBottom: 6,
+              }}>
+              {timeLeft.hours}hrs {timeLeft.min}mins {timeLeft.sec}secs{' '}
+            </Text>
+          </View>
+          <View
+            style={{
+              borderBottomWidth: 2,
+              width: `100%`,
+              alignItems: `center`,
+              borderBottomColor: `#509083`,
+              marginBottom: 16,
+            }}>
+            <Text
+              style={{
+                fontFamily: 'FredokaOne-Regular',
+                fontSize: 24,
+                color: `#509083`,
+              }}>
+              Time to Complete
+            </Text>
+            <Text
+              style={{
+                color: `#509083`,
+                fontSize: 18,
+                marginBottom: 6,
+              }}>
+              {timeLeft.time}
+            </Text>
+          </View>
+        </View>
+      ) : (
+        <View></View>
+      )}
+
       {/* {timeComponents.length > 0 ? (<View>{timeComponents} </View>):(<View></View>) } */}
       {/* {timeLeft ? () : (<Text></Text>)} */}
     </Card>
-  )
-}
+  );
+};
 
-export default Timer
+export default Timer;
